b6a540f2eeabc4e8b4281653291f347728112856
LIM HYUN SEOK <cr3ux53c@gmail.com>
2019-09-06 15:35:09 +0900

    누락된 라이브러리 추가
    javascript, css ...

--- knut_festival/settings.py
+++ knut_festival/settings.py
@@ -120,9 +120,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'app', 'static')
-] #static 파일들이 어디에 있는지 쓰는곳
-
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'app', 'static')
+# ] #static 파일들이 어디에 있는지 쓰는곳
+#
+# STATIC_ROOT = os.path.join(BASE_DIR,'static')
 # static 파일들이 어디로 모일 것인지 쓰는곳
