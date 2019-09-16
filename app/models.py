52367aeaa7383fa0e373b9a055316a33d3c9774c
ckr3453 <ckr3453@gmail.com>
2019-09-17 01:35:51 +0900

    ADD 'ImageField 'attributes.

--- app/models.py
+++ app/models.py
@@ -14,7 +14,7 @@ class Board(models.Model):
     user = models.CharField(max_length=10)
     body = models.TextField()
     created_at = models.DateTimeField('date published')
-    photo = models.ImageField(upload_to='images/')
+    photo = models.ImageField(upload_to='images/', blank=True, null=True)
 
     def __str__(self):
         return self.title
