cc770d90d3e66a38bfbfdc1470c9d2457fd7ba47
knut-sdpark <sdp1422@naver.com>
2019-09-17 02:28:13 +0900

    added 'forms.py' - 'BoardForm'

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,15 @@
 from django import forms
 from .models import Comment
+from .models import Board
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('user','text',)
 
+
+class BoardForm(forms.ModelForm):
+    class Meta:
+        model = Board
+
+        fields = ['title', 'body', 'photo']
