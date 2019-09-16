37a45517ab41f5a926a940f1bc74f7bf44134611
ckr3453 <ckr3453@gmail.com>
2019-09-16 22:25:54 +0900

    ADD 'pagination' func, 'create posts' func

--- app/views.py
+++ app/views.py
@@ -2,15 +2,13 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from .models import Comment, Board
 from .forms import CommentForm
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 def home(request):
     comments = Comment.objects.all()
-
-    boards = Board.objects.all()
-
-    return render(request, 'index.html', {'comments':comments, 'boards':boards})
+    return render(request, 'index.html', {'comments':comments})
 
 
 def comment_write(request):
@@ -29,15 +27,35 @@ def comment_write(request):
 
     return render(request, 'index.html', {'form':form, 'comments':comments})
 
-def login(request):
-    return render(request, 'index.html')
 
-    # return render(request, 'index.html', {'form':form, 'comments':comments})
+# def login(request):
+#     return render(request, 'index.html')
 
+
+
+# primary key 값을 부여해서 게시글 마다 고유한 번호를 가질수 있게 설계 (게시글 구분)
 def detail(request, board_id):
     board_detail = get_object_or_404(Board, pk=board_id)
     return render(request, 'detail.html', {'board' : board_detail})
 
 def board(request):
-    boards = Board.objects.all()
-    return render(request, 'board.html', {'boards':boards})
+
+    boards = Board.objects
+    boards_list = Board.objects.all().order_by('-created_at')
+    paginator = Paginator(boards_list, 5) # 게시물 5개를 기준으로 페이지네이션 전개
+    page = request.GET.get('page')        # request 된 페이지를 변수에 담음
+    posts = paginator.get_page(page)
+
+    return render(request, 'board.html', {'boards':boards, 'posts':posts})
+
+def new(request):
+    return render(request, 'new.html')
+
+# 제출 버튼을 누른뒤 작성한 게시글 detail로 보내기 위한 함수
+def create(request):
+    board = Board()
+    board.title = request.GET['title']
+    board.body = request.GET['body']
+    board.created_at = timezone.datetime.now()
+    board.save()
+    return redirect('/board/')
