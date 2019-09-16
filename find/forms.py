460f7abba9d801aa2b290abe5b0df9af15bc3f97
ckr3453 <ckr3453@gmail.com>
2019-09-16 17:52:44 +0900

    delete 'find' app. - no use.

--- find/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Lost
-
-class NewLost(forms.ModelForm):  
-    photo = forms.ImageField()       
-    class Meta:
-        model = Lost
-        fields=['title','photo','body',] 
