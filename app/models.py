e27290547b89c8aec7c65d3c10d1d4c593038b19
ckr3453 <ckr3453@gmail.com>
2019-09-17 00:19:45 +0900

    ADD 'photo' field

--- app/models.py
+++ app/models.py
@@ -14,6 +14,7 @@ class Board(models.Model):
     user = models.CharField(max_length=10)
     body = models.TextField()
     created_at = models.DateTimeField('date published')
+    photo = models.ImageField(upload_to='images/')
 
     def __str__(self):
         return self.title
